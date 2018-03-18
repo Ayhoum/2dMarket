ac3eee8b977885f3ee0d3f9a116a833923c1a198
Alaa Semsemea <semsemea.a@gmail.com>
2018-03-18 20:33:57 +0100

    Alaa's UPDATES

--- en/scripts/index_1.php
+++ en/scripts/index_1.php
@@ -87,7 +87,8 @@ if (mysqli_num_rows($ad_result) > 0) {
             <div class="category-grid-box-1">
                 <!-- Image Box -->
                 <div class="image">
-                    <img alt="Tour Package" src="<?php echo $pic . $pic_name;?>" class="img-responsive" style="width: 313px; height: 234px">
+                    <img alt="Tour Package" src="<?php echo $pic . $pic_name;?>" class="img-responsive" style="width: 100%; height: 200px; object-fit: cover;">
+
                     <div class="price-tag">
                         <div class="price"><span><?php
                                 if ($selling_type=="BID"){
