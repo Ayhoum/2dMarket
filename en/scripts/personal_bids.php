f9470b44f3583406a3e66533a2414e427d776158
Alaa Semsemea <semsemea.a@gmail.com>
2018-03-26 23:48:01 +0200

    Alaa's UPDATES

--- en/scripts/personal_bids.php
+++ en/scripts/personal_bids.php
@@ -1,113 +1,113 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Alaa
- * Date: 15-3-2018
- * Time: 20:06
- */
 include 'sessions.php';
 //include '../../scripts/db_connection.php';
 //session_start();
 ob_start();
 ?>
 <div class="posts-masonry">
-        <ul class="list-unstyled">
-        <?php
-        $user_id    = $_SESSION['id'];
+    <ul class="list-unstyled">
+<?php
+$user_id    = $_SESSION['id'];
 
-        $ad_query = "SELECT * FROM `ADVERTISEMENT` WHERE `Selling_type` = 'BID' && `USER_id` = '{$user_id}' ";
+$bid_query = "SELECT * FROM `BID` WHERE `USER_id` = '{$user_id}'";
+$bid_result = mysqli_query($mysqli,$bid_query);
+if (mysqli_num_rows($bid_result)) {
+    while ($row = mysqli_fetch_assoc($bid_result)) {
+        $id = $row['id'];
+        $amount = $row['amount'];
+        $time = $row['time'];
+        $auction_id = $row['AUCTION_id'];
+        $ad_id = $row['ad_id'];
+
+        echo $ad_id . " | ". $amount . " | ". $time ;
+
+
+
+
+        $ad_query = "SELECT * FROM `ADVERTISEMENT` WHERE `id` = '{$ad_id}'";
         $ad_result = mysqli_query($mysqli, $ad_query);
-        while ($row= mysqli_fetch_assoc($ad_result)) {
-                    $ad_id = $row['id'];
-                    $title = $row['title'];
-                    $date = $row['date'];
-                    $price = $row['price'];
-                    $description = $row['description'];
-                    $condition = $row['condition'];
-                    $visits = $row['visits'];
+        while ($row = mysqli_fetch_assoc($ad_result)) {
+            $ad_id = $row['id'];
+            $title = $row['title'];
+            $date = $row['date'];
+            $price = $row['price'];
+            $description = $row['description'];
+            $condition = $row['condition'];
+            $visits = $row['visits'];
 
-                    $status = $row['status'];
-                    $selling_type = $row['selling_type'];
-                    $delivery_type = $row['delivery_type'];
-                    $ad_type = $row['ad_type'];
+            $status = $row['status'];
+            $selling_type = $row['selling_type'];
+            $delivery_type = $row['delivery_type'];
+            $ad_type = $row['ad_type'];
 
-                    $user_id = $row['USER_id'];
-                    $category_id = $row['CATEGORY_id'];
+            $user_id = $row['USER_id'];
+            $category_id = $row['CATEGORY_id'];
 
 
-                    //extract date:
-                    $date = strtotime($date);
-                    $date = date('d/m/Y', $date);
+            //extract date:
+            $date = strtotime($date);
+            $date = date('d/m/Y', $date);
 
-                    // Category_info
-                    $cat_query = "SELECT * FROM `CATEGORY` WHERE `id` = '{$category_id}'";
-                    $cat_result = mysqli_query($mysqli, $cat_query);
-                    if (mysqli_num_rows($cat_result) > 0) {
-                        while ($row = mysqli_fetch_assoc($cat_result)) {
-                            $cat_name = $row['name'];
-                        }
-                    }
+            // Category_info
+            $cat_query = "SELECT * FROM `CATEGORY` WHERE `id` = '{$category_id}'";
+            $cat_result = mysqli_query($mysqli, $cat_query);
+            if (mysqli_num_rows($cat_result) > 0) {
+                while ($row = mysqli_fetch_assoc($cat_result)) {
+                    $cat_name = $row['name'];
+                }
+            }
 
 
 // Address info.
 
-                    $address_query = "SELECT  * FROM `ADDRESS` WHERE `USER_id` = {$user_id}";
-                    $address_result = mysqli_query($mysqli, $address_query);
-                    if (mysqli_num_rows($address_result) > 0) {
-                        while ($row = mysqli_fetch_assoc($address_result)) {
-                            $user_street_name = $row['street_name'];
-                            $user_postcode = $row['postcode'];
-                            $user_house_number = $row['house_number'];
-                            $user_region = $row['region'];
-                            $user_city = $row['city'];
-                            $user_country = $row['country'];
+            $address_query = "SELECT  * FROM `ADDRESS` WHERE `USER_id` = {$user_id}";
+            $address_result = mysqli_query($mysqli, $address_query);
+            if (mysqli_num_rows($address_result) > 0) {
+                while ($row = mysqli_fetch_assoc($address_result)) {
+                    $user_street_name = $row['street_name'];
+                    $user_postcode = $row['postcode'];
+                    $user_house_number = $row['house_number'];
+                    $user_region = $row['region'];
+                    $user_city = $row['city'];
+                    $user_country = $row['country'];
 
-                            $location = $user_city ." | " . $user_country;
+                    $location = $user_city . " | " . $user_country;
 
-                        }
-                    } else {
-                       $location= " Unknown Location ! ";
-                    }
+                }
+            } else {
+                $location = " Unknown Location ! ";
+            }
 
-                    $image_query = "SELECT * FROM `ADVERTISEMENT_PICTURE` WHERE  `ADVERTISEMENT_id` = '{$ad_id}'";
-                    $image_result = mysqli_query($mysqli, $image_query);
-                    while ($row = mysqli_fetch_assoc($image_result)) {
-                        $pic = $row['picture_url'];
-                        $pic_name = $row['picture_name'];
-                    }
+            $image_query = "SELECT * FROM `ADVERTISEMENT_PICTURE` WHERE  `ADVERTISEMENT_id` = '{$ad_id}'";
+            $image_result = mysqli_query($mysqli, $image_query);
+            while ($row = mysqli_fetch_assoc($image_result)) {
+                $pic = $row['picture_url'];
+                $pic_name = $row['picture_name'];
+            }
 
-                    $image_num_query = "SELECT COUNT(*) AS 'IMG_COUNT' from ADVERTISEMENT_PICTURE WHERE `ADVERTISEMENT_id` = '{$ad_id}'";
-                    $image_num_result = mysqli_query($mysqli, $image_num_query);
-                    while ($row = mysqli_fetch_assoc($image_num_result)){
-                        $image_num = $row['IMG_COUNT'];
-                    }
-
-
-
-            $bid_query = "SELECT * FROM `BID` WHERE `USER_id` = '{$user_id}' && `ad_id` ='{$ad_id}' ORDER BY `amount`";
-            $bid_result = mysqli_query($mysqli,$bid_query);
-            while ($row= mysqli_fetch_assoc($bid_result)) {
-                $id = $row['id'];
-                $amount = $row['amount'];
-                $time = $row['time'];
+            $image_num_query = "SELECT COUNT(*) AS 'IMG_COUNT' from ADVERTISEMENT_PICTURE WHERE `ADVERTISEMENT_id` = '{$ad_id}'";
+            $image_num_result = mysqli_query($mysqli, $image_num_query);
+            while ($row = mysqli_fetch_assoc($image_num_result)) {
+                $image_num = $row['IMG_COUNT'];
+            }
 
 
-        // check if the user has the highest amount :
-                $bid_amount_query  = "SELECT * FROM `BID` WHERE `ad_id` ='{$ad_id}' ORDER BY `amount`";
-                $bid_amount_result = mysqli_query($mysqli, $bid_amount_query);
-                while ($row = mysqli_fetch_assoc($bid_amount_result)) {
-                    $am_id = $row['id'];
-                    $am_amount = $row['amount'];
-                    $am_date = $row['time'];
+            // check if the user has the highest amount :
+            $bid_amount_query = "SELECT * FROM `BID` WHERE `ad_id` ='{$ad_id}' ORDER BY `amount`";
+            $bid_amount_result = mysqli_query($mysqli, $bid_amount_query);
+            while ($row = mysqli_fetch_assoc($bid_amount_result)) {
+                $am_id = $row['id'];
+                $am_amount = $row['amount'];
+                $am_date = $row['time'];
 
-                    require_once 'time_elapse.php';
-                    $am_date = time_elapsed_string($am_date);
+                require_once 'time_elapse.php';
+                $am_date = time_elapsed_string($am_date);
+            }
 
 
+            ?>
 
-                    ?>
-                                <!-- Listing Grid -->
-                                <li>
+            <li>
                                     <div class="well ad-listing clearfix">
                                         <div class="col-md-3 col-sm-5 col-xs-12 grid-style no-padding">
                                             <!-- Image Box -->
@@ -158,15 +158,15 @@ ob_start();
                                         </div>
                                     </div>
                                 </li>
-                    <?php
-                }
 
-            }
-
+            <?php
 
         }
-        ?>
-        </ul>
 
-</div>
+    }
 
+}
+
+
+
+?>
