586ab5877732e5d0381a7e80f54ee6dbaea1a26a
Alaa Semsemea <semsemea.a@gmail.com>
2018-03-27 00:22:01 +0200

    Alaa's UPDATES

--- en/scripts/add_to_favorite.php
+++ en/scripts/add_to_favorite.php
@@ -18,12 +18,12 @@ $ad_id = $_GET['ad_id'];
 $check_query = "SELECT * FROM `FAV_ADS` WHERE `user_id` = '{$user_id}'&& `ad_id` = '{$ad_id}'";
 $check_result = mysqli_query($mysqli,$check_query);
 if (mysqli_num_rows($check_result) > 0 ){
-    header("Location : ../favourite_ads.php");
+    header("Location: " . $_SERVER["HTTP_REFERER"]);
 
 } else{
     $add_query  = "INSERT INTO `FAV_ADS` (`user_id`,`ad_id`) VALUES ('{$user_id}','{$ad_id}')";
     $add_result = mysqli_query($mysqli, $add_query);
 
-    header("Location : ../favourite_ads.php");
- }
+    header("Location: " . $_SERVER["HTTP_REFERER"]);
+}
 ?>
