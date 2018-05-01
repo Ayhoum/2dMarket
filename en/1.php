3afb719ae23589612f03cffcfa5aa62b3f62a682
Alaa Semsemea <semsemea.a@gmail.com>
2018-05-02 00:16:41 +0200

    Alaa's UPDATES

---
+++ en/1.php
@@ -0,0 +1,268 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alaa
+ * Date: 1-5-2018
+ * Time: 23:58
+ */
+include '../scripts/db_connection.php'
+?>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+      <!--[if IE]>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <![endif]-->
+      <meta name="description" content="">
+      <meta name="author" content="ScriptsBundle">
+      <title>AdForest | Largest Classifieds Portal</title>
+      <!-- =-=-=-=-=-=-= Favicons Icon =-=-=-=-=-=-= -->
+      <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+      <!-- =-=-=-=-=-=-= Mobile Specific =-=-=-=-=-=-= -->
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+      <!-- =-=-=-=-=-=-= Bootstrap CSS Style =-=-=-=-=-=-= -->
+      <link rel="stylesheet" href="css/bootstrap.css">
+      <!-- =-=-=-=-=-=-= Template CSS Style =-=-=-=-=-=-= -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- =-=-=-=-=-=-= Font Awesome =-=-=-=-=-=-= -->
+      <link rel="stylesheet" href="css/font-awesome.css" type="text/css">
+      <!-- =-=-=-=-=-=-= Flat Icon =-=-=-=-=-=-= -->
+      <link href="css/flaticon.css" rel="stylesheet">
+      <!-- =-=-=-=-=-=-= Et Line Fonts =-=-=-=-=-=-= -->
+      <link rel="stylesheet" href="css/et-line-fonts.css" type="text/css">
+      <!-- =-=-=-=-=-=-= Menu Drop Down =-=-=-=-=-=-= -->
+      <link rel="stylesheet" href="css/forest-menu.css" type="text/css">
+      <!-- =-=-=-=-=-=-= Animation =-=-=-=-=-=-= -->
+      <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+      <!-- =-=-=-=-=-=-= Select Options =-=-=-=-=-=-= -->
+      <link href="css/select2.min.css" rel="stylesheet" />
+      <!-- =-=-=-=-=-=-= noUiSlider =-=-=-=-=-=-= -->
+      <link href="css/nouislider.min.css" rel="stylesheet">
+      <!-- =-=-=-=-=-=-= Listing Slider =-=-=-=-=-=-= -->
+      <link href="css/slider.css" rel="stylesheet">
+      <!-- =-=-=-=-=-=-= Owl carousel =-=-=-=-=-=-= -->
+      <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+      <link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+      <!-- =-=-=-=-=-=-= Check boxes =-=-=-=-=-=-= -->
+      <link href="skins/minimal/minimal.css" rel="stylesheet">
+      <!-- =-=-=-=-=-=-= Responsive Media =-=-=-=-=-=-= -->
+      <link href="css/responsive-media.css" rel="stylesheet">
+      <!-- =-=-=-=-=-=-= Template Color =-=-=-=-=-=-= -->
+      <link rel="stylesheet" id="color" href="css/colors/defualt.css">
+      <!-- =-=-=-=-=-=-= For Style Switcher =-=-=-=-=-=-= -->
+      <link rel="stylesheet" id="theme-color" type="text/css" href="#" />
+      <!-- JavaScripts -->
+      <script src="js/modernizr.js"></script>
+      <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+   </head>
+   <body>
+      <!-- =-=-=-=-=-=-= Preloader =-=-=-=-=-=-= -->
+      <div id="loader-wrapper">
+          <div id="loader"><img class="img-responsive"  src="images/logo_files/design.gif">
+              <h4 class="text-center" style="color: #00a9da"> Loading..</h4> </div>
+          <div class="loader-section section-left"></div>
+          <div class="loader-section section-right"></div>
+      </div>
+     <!-- =-=-=-=-=-=-= Color Switcher =-=-=-=-=-=-= -->
+
+      <!-- =-=-=-=-=-=-= Light Header =-=-=-=-=-=-= -->
+      <div class="colored-header">
+          <!-- Top Bar -->
+          <?php include 'topbar-en.php';?>
+          <!-- Top Bar End -->
+          <!-- Navigation Menu -->
+
+          <?php include "nav_bar_en.php";?>
+      </div>
+      <!-- Small Breadcrumb -->
+      <!-- =-=-=-=-=-=-= Transparent Breadcrumb End =-=-=-=-=-=-= -->
+      <!-- =-=-=-=-=-=-= Main Content Area =-=-=-=-=-=-= -->
+      <div class="main-content-area clearfix">
+         <!-- =-=-=-=-=-=-= Featured Listing =-=-=-=-=-=-= -->
+         <section class="custom-padding gray">
+            <!-- Main Container -->
+            <div class="container">
+               <!-- Row -->
+               <div class="row">
+
+                  <!-- Middle Content Box -->
+                  <div class="col-md-12 col-xs-12 col-sm-12 ">
+                     <div class="row">
+                        <!-- Category List -->
+                         <ul class="category-list-style">
+                             <!-- Category -->
+                             <!-- Category List -->
+                             <?php
+
+                             $query = "SELECT * FROM `CATEGORY` WHERE `lang` = 'EN'";
+                             $result = mysqli_query($mysqli, $query);
+                             While($row = mysqli_fetch_assoc($result)){
+                                 $id = $row['id'];
+                                 $name = $row['name'];
+                                 $icon = $row['icon_name'];
+
+                                 $count_query = "SELECT COUNT(*) AS 'CAT_count' FROM `ADVERTISEMENT` WHERE CATEGORY_id = '{$id}' ";
+                                 $count_result = mysqli_query($mysqli,$count_query);
+                                 while ($row = mysqli_fetch_assoc($count_result)){
+                                     $cat_count = $row['CAT_count'];
+                                 }
+                                 ?>
+
+                                 <li class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                                     <a href="ad_per_cat.php?cat_id=<?php echo $id; ?>"><?php echo $name;?><span>(<?php echo $cat_count; ?> Ads)</span>
+                                         <i class="<?php echo $icon;?>"></i>
+                                     </a>
+                                 </li>
+
+                             <?php }?>
+                         </ul>
+                        <!-- Category List End -->
+                     </div>
+                  </div>
+                  <!-- Middle Content Box End -->
+               </div>
+               <!-- Row End -->
+            </div>
+            <!-- Main Container End -->
+         </section>
+         <!-- =-=-=-=-=-=-= Featured Listing End =-=-=-=-=-=-= -->
+         <!-- =-=-=-=-=-=-= FOOTER =-=-=-=-=-=-= -->
+         <footer>
+            <!-- Footer Content -->
+            <div class="footer-top">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-3  col-sm-6 col-xs-12">
+                        <!-- Info Widget -->
+                        <div class="widget">
+                           <div class="logo"> <img alt="" src="images/logo-1.png"> </div>
+                           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et dolor eget erat fringilla port.</p>
+                           <ul>
+                              <li><img src="images/appstore.png" alt=""></li>
+                              <li><img src="images/googleplay.png" alt=""></li>
+                           </ul>
+                        </div>
+                        <!-- Info Widget Exit -->
+                     </div>
+                     <div class="col-md-3  col-sm-6 col-xs-12">
+                        <!-- Follow Us -->
+                        <div class="widget socail-icons">
+                           <h5>Follow Us</h5>
+                           <ul>
+                              <li><a class="fb" href=""><i class="fa fa-facebook"></i></a><span>Facebook</span></li>
+                              <li><a class="twitter" href=""><i class="fa fa-twitter"></i></a><span>Twitter</span></li>
+                              <li><a class="linkedin" href=""><i class="fa fa-linkedin"></i></a><span>Linkedin</span></li>
+                              <li><a class="googleplus" href=""><i class="fa fa-google-plus"></i></a><span>Google+</span></li>
+                           </ul>
+                        </div>
+                        <!-- Follow Us End -->
+                     </div>
+                     <div class="col-md-6  col-sm-6 col-xs-12">
+                        <!-- Newslatter -->
+                        <div class="widget widget-newsletter">
+                           <h5>Singup for Weekly Newsletter</h5>
+                           <div class="fieldset">
+                              <p>We may send you information about related events, webinars, products and services which we believe.</p>
+                              <form>
+                                 <input class="" value="Enter your email address" type="text">
+                                 <input class="submit-btn" name="submit" value="Submit" type="submit">
+                              </form>
+                           </div>
+                        </div>
+                        <!-- Newslatter -->
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <!-- Copyrights -->
+            <div class="copyrights">
+               <div class="container">
+                  <div class="copyright-content">
+                     <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                           <p>© 2017 AForest All rights reserved. Design by <a href="http://themeforest.net/user/scriptsbundle/portfolio" target="_blank">Scriptsbundle</a> </p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </footer>
+         <!-- =-=-=-=-=-=-= FOOTER END =-=-=-=-=-=-= -->
+      </div>
+      <!-- Main Content Area End -->
+      <!-- Post Ad Sticky -->
+      <a href="#" class="sticky-post-button hidden-xs">
+         <span class="sell-icons">
+         <i class="flaticon-transport-9"></i>
+         </span>
+         <h4>SELL</h4>
+      </a>
+      <!-- Back To Top -->
+      <a href="#0" class="cd-top">Top</a>
+      <!-- Back To Top -->
+      <div class="custom-modal">
+         <div id="myModal" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+               <!-- Modal content-->
+               <div class="modal-content">
+                  <div class="modal-header rte">
+                     <h2 class="modal-title">Forgot Your Password ?</h2>
+                  </div>
+                  <form>
+                     <div class="modal-body">
+                        <div class="form-group">
+                           <label>Email</label>
+                           <input placeholder="Enter Your Email Adress" class="form-control" type="email">
+                        </div>
+                     </div>
+                     <div class="modal-footer">
+                        <button type="button" class="btn btn-default">Reset My Account</button>
+                        <button type="button" class="btn btn-dark" data-dismiss="modal">Cancel</button>
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- =-=-=-=-=-=-= JQUERY =-=-=-=-=-=-= -->
+      <script src="js/jquery.min.js"></script>
+      <!-- Bootstrap Core Css  -->
+      <script src="js/bootstrap.min.js"></script>
+      <!-- Jquery Easing -->
+      <script src="js/easing.js"></script>
+      <!-- Menu Hover  -->
+      <script src="js/forest-megamenu.js"></script>
+      <!-- Jquery Appear Plugin -->
+      <script src="js/jquery.appear.min.js"></script>
+      <!-- Numbers Animation   -->
+      <script src="js/jquery.countTo.js"></script>
+      <!-- Jquery Smooth Scroll  -->
+      <script src="js/jquery.smoothscroll.js"></script>
+      <!-- Jquery Select Options  -->
+      <script src="js/select2.min.js"></script>
+      <!-- noUiSlider -->
+      <script src="js/nouislider.all.min.js"></script>
+      <!-- Carousel Slider  -->
+      <script src="js/carousel.min.js"></script>
+      <script src="js/slide.js"></script>
+      <!-- Image Loaded  -->
+      <script src="js/imagesloaded.js"></script>
+      <script src="js/isotope.min.js"></script>
+      <!-- CheckBoxes  -->
+      <script src="js/icheck.min.js"></script>
+      <!-- Jquery Migration  -->
+      <script src="js/jquery-migrate.min.js"></script>
+      <!-- Sticky Bar  -->
+      <script src="js/theia-sticky-sidebar.js"></script>
+      <!-- Style Switcher -->
+      <script src="js/color-switcher.js"></script>
+      <!-- Template Core JS -->
+      <script src="js/custom.js"></script>
+   </body>
+</html>
