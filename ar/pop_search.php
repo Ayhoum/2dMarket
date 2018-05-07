ab33de5120e8597a8621bb77c6812496fe144263
Alaa Semsemea <semsemea.a@gmail.com>
2018-05-07 02:48:18 +0200

    Alaa's UPDATES

--- ar/pop_search.php
+++ ar/pop_search.php
@@ -135,7 +135,7 @@ include '../scripts/db_connection.php';
                                     <!-- Title End -->
                                 </div>
                                 <!-- Content -->
-                                <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                                <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
                                     <div class="panel-body categories">
                                         <ul>
                                             <?php
