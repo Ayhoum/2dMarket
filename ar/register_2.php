9fef7a15075c1d5fe1e2793830f7049995267f59
Ayham <aylosa@outlook.com>
2018-05-08 15:13:33 +0200

    Ayham

--- ar/register_2.php
+++ ar/register_2.php
@@ -62,13 +62,13 @@ if (isset($_POST['submit'])){
     $body             = "
 
 
-<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
-<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\">
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+<html xmlns='http://www.w3.org/1999/xhtml' xmlns:v='urn:schemas-microsoft-com:vml' xmlns:o='urn:schemas-microsoft-com:office:office'>
 <head>
-	<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />
-	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>
+	<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
+	<meta name='viewport' content='width=device-width, initial-scale=1.0'/>
 	<title>2D Market | تـأكيد تسجيـل الحسـاب</title>
-	<style type=\"text/css\">
+	<style type='text/css'>
 		/* ----- Custom Font Import ----- */
 		/*@import url(https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic&subset=latin,latin-ext);*/
 		@import url(https://fontlibrary.org/face/droid-arabic-kufi);
@@ -214,23 +214,23 @@ if (isset($_POST['submit'])){
 </xml><![endif]-->
 </head>
 
-<body style=\"padding: 0; margin: 0;\" bgcolor=\"#eeeeee\">
+<body style='padding: 0; margin: 0;' bgcolor='#eeeeee'>
 
 <!-- / Full width container -->
-<table class=\"full-width-container\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" height=\"100%\" width=\"100%\" bgcolor=\"#eeeeee\" style=\"width: 100%; height: 100%; padding: 30px 0 30px 0;\">
+<table class='full-width-container' border='0' cellpadding='0' cellspacing='0' height='100%' width='100%' bgcolor='#eeeeee' style='width: 100%; height: 100%; padding: 30px 0 30px 0;'>
 	<tr>
-		<td align=\"center\" valign=\"top\">
+		<td align='center' valign='top'>
 			<!-- / 700px container -->
-			<table class=\"container\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"700\" bgcolor=\"#ffffff\" style=\"width: 700px;\">
+			<table class='container' border='0' cellpadding='0' cellspacing='0' width='700' bgcolor='#ffffff' style='width: 700px;'>
 				<tr>
-					<td align=\"center\" valign=\"top\">
+					<td align='center' valign='top'>
 						<!-- / Header -->
-						<table class=\"container header\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"620\" style=\"width: 620px;\">
+						<table class='container header' border='0' cellpadding='0' cellspacing='0' width='620' style='width: 620px;'>
 							<tr>
-								<td class=\"info-bullets__block\" style=\"padding: 30px 30px 15px 30px;\" align=\"center\">
-									<table class=\"container\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" align=\"center\">
+								<td class='info-bullets__block' style='padding: 30px 30px 15px 30px;' align='center'>
+									<table class='container' border='0' cellpadding='0' cellspacing='0' align='center'>
 										<tr>
-											<td align=\"center\" width=\"60\" height=\"2\" style=\" width: 60px; height: 2px; font-size: 1px;\"><img src=\"http://www.2dmarket.com/en/images/logo_files/logo_png.png\"></td>
+											<td align='center' width='60' height='2' style=' width: 60px; height: 2px; font-size: 1px;'><img src='http://www.2dmarket.com/ar/images/logo_files/logo_png_email.png'></td>
 										</tr>
 									</table>
 								</td>
@@ -240,25 +240,25 @@ if (isset($_POST['submit'])){
 						<!-- /// Header -->
 
 						<!-- / Hero subheader -->
-						<table class=\"container hero-subheader\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"620\" style=\"width: 620px;\">
+						<table class='container hero-subheader' border='0' cellpadding='0' cellspacing='0' width='620' style='width: 620px;'>
 							<tr>
-								<td class=\"hero-subheader__title\" style=\"direction:ltr;font-size: 43px; font-weight: bold; padding: 80px 0 15px 0;\" align=\"center\">لتتمكن من تسجيل الدخول في موقعنا الرجـاء تأكيد حسابك</td>
+								<td class='hero-subheader__title' style='direction:ltr;font-size: 43px; font-weight: bold; padding: 80px 0 15px 0;' align='center'>لتتمكن من تسجيل الدخول في موقعنا الرجـاء تأكيد حسابك</td>
 							</tr>
 
 							<tr>
-								<td class=\"hero-subheader__content\" style=\"direction:ltr;font-size: 16px; line-height: 27px; color: #969696; padding: 0 60px 90px 0;\" align=\"right\">لتأكيـد الحسـاب :<br>
+								<td class='hero-subheader__content' style='direction:ltr;font-size: 16px; line-height: 27px; color: #969696; padding: 0 60px 90px 0;' align='right'>لتأكيـد الحسـاب :<br>
 								<a href='$link'>اضغط هنـا</a>
 								</td>
 							</tr>
 						</table>
 
 						<!-- / Divider -->
-						<table class=\"container\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"padding-top: 25px;\" align=\"center\">
+						<table class='container' border='0' cellpadding='0' cellspacing='0' width='100%' style='padding-top: 25px;' align='center'>
 							<tr>
-								<td align=\"center\">
-									<table class=\"container\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"620\" align=\"center\" style=\"border-bottom: solid 1px #eeeeee; width: 620px;\">
+								<td align='center'>
+									<table class='container' border='0' cellpadding='0' cellspacing='0' width='620' align='center' style='border-bottom: solid 1px #eeeeee; width: 620px;'>
 										<tr>
-											<td align=\"center\">&nbsp;</td>
+											<td align='center'>&nbsp;</td>
 										</tr>
 									</table>
 								</td>
@@ -267,19 +267,19 @@ if (isset($_POST['submit'])){
 						<!-- /// Divider -->
 
 						<!-- / Info Bullets -->
-						<table class=\"container info-bullets\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" align=\"center\">
+						<table class='container info-bullets' border='0' cellpadding='0' cellspacing='0' width='100%' align='center'>
 							<tr>
-								<td align=\"center\">
-									<table class=\"container\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"620\" align=\"center\" style=\"width: 620px;\">
+								<td align='center'>
+									<table class='container' border='0' cellpadding='0' cellspacing='0' width='620' align='center' style='width: 620px;'>
 										<tr>
-											<td class=\"info-bullets__block\" style=\"padding: 30px 30px 15px 30px;\" align=\"center\">
-												<table class=\"container\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" align=\"center\">
+											<td class='info-bullets__block' style='padding: 30px 30px 15px 30px;' align='center'>
+												<table class='container' border='0' cellpadding='0' cellspacing='0' align='center'>
 													<tr>
-														<td class=\"info-bullets__icon\" style=\"padding: 0 15px 0 0;\">
-															<img src=\"https://cdn1.iconfinder.com/data/icons/material-communication/20/email-32.png\">
+														<td class='info-bullets__icon' style='padding: 0 15px 0 0;'>
+															<img src='https://cdn1.iconfinder.com/data/icons/material-communication/20/email-32.png'>
 														</td>
 
-														<td class=\"info-bullets__content\" style=\"color: #969696; font-size: 16px;\">info@2dmarket.com</td>
+														<td class='info-bullets__content' style='color: #969696; font-size: 16px;'>info@2dmarket.com</td>
 													</tr>
 												</table>
 											</td>
@@ -291,20 +291,20 @@ if (isset($_POST['submit'])){
 						<!-- /// Info Bullets -->
 
 						<!-- / Social nav -->
-						<table class=\"container\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" align=\"center\">
+						<table class='container' border='0' cellpadding='0' cellspacing='0' width='100%' align='center'>
 							<tr>
-								<td align=\"center\">
-									<table class=\"container\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"620\" align=\"center\" style=\"border-top: 1px solid #eeeeee; width: 620px;\">
+								<td align='center'>
+									<table class='container' border='0' cellpadding='0' cellspacing='0' width='620' align='center' style='border-top: 1px solid #eeeeee; width: 620px;'>
 										<tr>
-											<td align=\"center\" style=\"padding: 30px 0 30px 0;\">
-												<a href=\"https://www.facebook.com/2Dmarket\">
-													<img src=\"https://cdn2.iconfinder.com/data/icons/social-media-2151/512/1_Media_social_website_facebook-48.png\" border=\"0\">
+											<td align='center' style='padding: 30px 0 30px 0;'>
+												<a href='https://www.facebook.com/2Dmarket'>
+													<img src='https://cdn2.iconfinder.com/data/icons/social-media-2151/512/1_Media_social_website_facebook-48.png' border='0'>
 												</a>
 											</td>
 
-											<td align=\"center\" style=\"padding: 30px 0 30px 0;\">
-												<a href=\"https://twitter.com/2DmarKet1\">
-													<img src=\"https://cdn2.iconfinder.com/data/icons/social-media-2151/512/11_Media_social_website_twitter-48.png\" border=\"0\">
+											<td align='center' style='padding: 30px 0 30px 0;'>
+												<a href='https://twitter.com/2DmarKet1'>
+													<img src='https://cdn2.iconfinder.com/data/icons/social-media-2151/512/11_Media_social_website_twitter-48.png' border='0'>
 												</a>
 											</td>
 										</tr>
@@ -315,28 +315,28 @@ if (isset($_POST['submit'])){
 						<!-- /// Social nav -->
 
 						<!-- / Footer -->
-						<table class=\"container\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" align=\"center\">
+						<table class='container' border='0' cellpadding='0' cellspacing='0' width='100%' align='center'>
 							<tr>
-								<td align=\"center\">
-									<table class=\"container\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"620\" align=\"center\" style=\"border-top: 1px solid #eeeeee; width: 620px;\">
+								<td align='center'>
+									<table class='container' border='0' cellpadding='0' cellspacing='0' width='620' align='center' style='border-top: 1px solid #eeeeee; width: 620px;'>
 										<tr>
-											<td style=\"text-align: center; padding: 50px 0 10px 0;\">
-												<a href=\"#\" style=\"font-size: 28px; text-decoration: none; color: #d5d5d5;\">2D Market</a>
+											<td style='text-align: center; padding: 50px 0 10px 0;'>
+												<a href='#' style='font-size: 28px; text-decoration: none; color: #d5d5d5;'>2D Market</a>
 											</td>
 										</tr>
 
 										<tr>
-											<td align=\"middle\">
-												<table width=\"60\" height=\"2\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"width: 60px; height: 2px;\">
+											<td align='middle'>
+												<table width='60' height='2' border='0' cellpadding='0' cellspacing='0' style='width: 60px; height: 2px;'>
 													<tr>
-														<td align=\"middle\" width=\"60\" height=\"2\" style=\" width: 60px; height: 2px; font-size: 1px;\"><img src=\"http://www.2dmarket.com/en/images/logo_png.png\"></td>
+														<td align='middle' width='60' height='2' style=' width: 60px; height: 2px; font-size: 1px;'><img src='http://www.2dmarket.com/en/images/logo_png.png'></td>
 													</tr>
 												</table>
 											</td>
 										</tr>
 
 										<tr>
-											<td style=\"color: #d5d5d5; text-align: center; font-size: 15px; padding: 10px 0 60px 0; line-height: 22px;\">Copyright &copy; 2018 <a href=\"http://www.2dmarket.com/\" target=\"_blank\" style=\"text-decoration: none; border-bottom: 1px solid #d5d5d5; color: #d5d5d5;\">2D Market</a>. <br />All rights reserved.</td>
+											<td style='color: #d5d5d5; text-align: center; font-size: 15px; padding: 10px 0 60px 0; line-height: 22px;'>Copyright &copy; 2018 <a href='http://www.2dmarket.com/' target='_blank' style='text-decoration: none; border-bottom: 1px solid #d5d5d5; color: #d5d5d5;'>2D Market</a>. <br />All rights reserved.</td>
 										</tr>
 									</table>
 								</td>
