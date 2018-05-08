1ddf0240f4633c6d01b4a6a5a63f02c02f78b4a7
Alaa Semsemea <semsemea.a@gmail.com>
2018-05-09 00:11:19 +0200

    Alaa's UPDATES

--- en/scripts/delete_ad.php
+++ en/scripts/delete_ad.php
@@ -10,18 +10,21 @@
 require_once '../../scripts/db_connection.php';
 $ad_id = $_GET['ad_id'];
 
+
 // delete from auction
 $delete_auction_query  = "DELETE FROM `AUCTION` WHERE  `ADVERTISEMENT_id` = '{$ad_id}'";
 $delete_auction_result = mysqli_query($mysqli, $delete_auction_query);
+
 
 //delete from pics
 $delete_pic_query  = "DELETE FROM `ADVERTISEMENT_PICTURE` WHERE `ADVERTISEMENT_id` = '{$ad_id}' ";
 $delete_pic_result = mysqli_query($mysqli,$delete_pic_query);
 
+
 //delete ad.
 $delete_query = "DELETE FROM `ADVERTISEMENT` WHERE id = '{$ad_id}'";
-
 $delete_result = mysqli_query($mysqli, $delete_query);
+
 
 
 echo 'done';
