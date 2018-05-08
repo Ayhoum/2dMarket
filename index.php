6c7d9e44d7a9bf3f24317a00530160297b70cdac
Ayham <aylosa@outlook.com>
2018-05-08 04:03:08 +0200

    Ayham

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-header("Location: en/index.php");
-?>
+<?php ob_start(); ?>
+<html>
+<head>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+   <script>
+       (adsbygoogle = window.adsbygoogle || []).push({
+           google_ad_client: "ca-pub-5850194650079393",
+           enable_page_level_ads: true
+       });
+	</script>
+</head>
+<?php header("Location: en/index.php") ?>
+</html>
