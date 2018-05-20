99502e4ac6cf49ff9d49553d9d1ff421a0973756
Alaa Semsemea <semsemea.a@gmail.com>
2018-05-20 15:36:18 +0200

    Alaa's UPDATES

--- ar/scripts/ad_page_script.php
+++ ar/scripts/ad_page_script.php
@@ -5,6 +5,7 @@
  * Date: 4-3-2018
  * Time: 21:37
  */
+include '../phpmailer/class.phpmailer.php';
 ?>
 <?php
 if (isset($_GET['ad_id'])) {
@@ -270,6 +271,334 @@ if (isset($_GET['ad_id'])) {
 
                 }
             }
+
+            $ad_query = "SELECT * FROM `ADVERTISEMENT` WHERE `id` = {$ad_id}";
+            $ad_result = mysqli_query($mysqli, $ad_query);
+
+            if (mysqli_num_rows($ad_result) > 0) {
+                while ($row = mysqli_fetch_assoc($ad_result)) {
+                    $user_id1 = $row['USER_id'];
+                }
+                $email_query = "SELECT * FROM `USER` WHERE `id` = '{$user_id1}'";
+                $email_result = mysqli_query($mysqli, $email_query);
+                while ($row = mysqli_fetch_assoc($email_result)) {
+
+                    $email_to_send = $row['email'];
+
+                }
+            }
+
+            $mail             = new PHPMailer(); // defaults to using php "mail()"
+            $mail->CharSet = 'UTF-8';
+            $mail->IsHTML(true);
+            $link = "http://www.2dmarket.com/ar/ad_page.php?".$ad_id;
+
+            $body             = "
+
+
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+<html xmlns='http://www.w3.org/1999/xhtml' xmlns:v='urn:schemas-microsoft-com:vml' xmlns:o='urn:schemas-microsoft-com:office:office'>
+<head>
+	<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
+	<meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+	<title>2D Market | مزايدة جديدة</title>
+	<style type='text/css'>
+		/* ----- Custom Font Import ----- */
+		/*@import url(https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic&subset=latin,latin-ext);*/
+		@import url(https://fontlibrary.org/face/droid-arabic-kufi);
+
+		/* ----- Text Styles ----- */
+		table{
+			font-family: 'DroidArabicKufiRegular';
+			-webkit-font-smoothing: antialiased;
+			-moz-font-smoothing: antialiased;
+			font-smoothing: antialiased;
+		}
+
+		@media only screen and (max-width: 700px){
+			/* ----- Base styles ----- */
+			.full-width-container{
+				padding: 0 !important;
+			}
+
+			.container{
+				width: 100% !important;
+			}
+
+			/* ----- Header ----- */
+			.header td{
+				padding: 30px 15px 30px 15px !important;
+			}
+
+			/* ----- Projects list ----- */
+			.projects-list{
+				display: block !important;
+			}
+
+			.projects-list tr{
+				display: block !important;
+			}
+
+			.projects-list td{
+				display: block !important;
+			}
+
+			.projects-list tbody{
+				display: block !important;
+			}
+
+			.projects-list img{
+				margin: 0 auto 25px auto;
+			}
+
+			/* ----- Half block ----- */
+			.half-block{
+				display: block !important;
+			}
+
+			.half-block tr{
+				display: block !important;
+			}
+
+			.half-block td{
+				display: block !important;
+			}
+
+			.half-block__image{
+				width: 100% !important;
+				background-size: cover;
+			}
+
+			.half-block__content{
+				width: 100% !important;
+				box-sizing: border-box;
+				padding: 25px 15px 25px 15px !important;
+			}
+
+			/* ----- Hero subheader ----- */
+			.hero-subheader__title{
+				padding: 80px 15px 15px 15px !important;
+				font-size: 35px !important;
+			}
+
+			.hero-subheader__content{
+				padding: 0 15px 90px 15px !important;
+			}
+
+			/* ----- Title block ----- */
+			.title-block{
+				padding: 0 15px 0 15px;
+			}
+
+			/* ----- Paragraph block ----- */
+			.paragraph-block__content{
+				padding: 25px 15px 18px 15px !important;
+			}
+
+			/* ----- Info bullets ----- */
+			.info-bullets{
+				display: block !important;
+			}
+
+			.info-bullets tr{
+				display: block !important;
+			}
+
+			.info-bullets td{
+				display: block !important;
+			}
+
+			.info-bullets tbody{
+				display: block;
+			}
+
+			.info-bullets__icon{
+				text-align: center;
+				padding: 0 0 15px 0 !important;
+			}
+
+			.info-bullets__content{
+				text-align: center;
+			}
+
+			.info-bullets__block{
+				padding: 25px !important;
+			}
+
+			/* ----- CTA block ----- */
+			.cta-block__title{
+				padding: 35px 15px 0 15px !important;
+			}
+
+			.cta-block__content{
+				padding: 20px 15px 27px 15px !important;
+			}
+
+			.cta-block__button{
+				padding: 0 15px 0 15px !important;
+			}
+		}
+	</style>
+
+	<!--[if gte mso 9]><xml>
+	<o:OfficeDocumentSettings>
+		<o:AllowPNG/>
+		<o:PixelsPerInch>96</o:PixelsPerInch>
+	</o:OfficeDocumentSettings>
+</xml><![endif]-->
+</head>
+
+<body style='padding: 0; margin: 0;' bgcolor='#eeeeee'>
+
+<!-- / Full width container -->
+<table class='full-width-container' border='0' cellpadding='0' cellspacing='0' height='100%' width='100%' bgcolor='#eeeeee' style='width: 100%; height: 100%; padding: 30px 0 30px 0;'>
+	<tr>
+		<td align='center' valign='top'>
+			<!-- / 700px container -->
+			<table class='container' border='0' cellpadding='0' cellspacing='0' width='700' bgcolor='#ffffff' style='width: 700px;'>
+				<tr>
+					<td align='center' valign='top'>
+						<!-- / Header -->
+						<table class='container header' border='0' cellpadding='0' cellspacing='0' width='620' style='width: 620px;'>
+							<tr>
+								<td class='info-bullets__block' style='padding: 30px 30px 15px 30px;' align='center'>
+									<table class='container' border='0' cellpadding='0' cellspacing='0' align='center'>
+										<tr>
+											<td align='center' width='60' height='2' style=' width: 60px; height: 2px; font-size: 1px;'><img src='http://www.2dmarket.com/ar/images/logo_files/logo_png_email.png'></td>
+										</tr>
+									</table>
+								</td>
+
+							</tr>
+						</table>
+						<!-- /// Header -->
+
+						<!-- / Hero subheader -->
+						<table class='container hero-subheader' border='0' cellpadding='0' cellspacing='0' width='620' style='width: 620px;'>
+							<tr>
+								<td class='hero-subheader__title' style='direction:ltr;font-size: 43px; font-weight: bold; padding: 80px 0 15px 0;' align='center'>لديك مزايدة جديدة على منتجك : </td>
+							</tr>
+
+							<tr>
+								<td class='hero-subheader__content' style='direction:ltr;font-size: 16px; line-height: 27px; color: #969696; padding: 0 60px 90px 0;' align='right'>لمشاهدة المزايدة :<br>
+								<a href='$link'>إضغط هنا!</a>
+								</td>
+							</tr>
+						</table>
+
+						<!-- / Divider -->
+						<table class='container' border='0' cellpadding='0' cellspacing='0' width='100%' style='padding-top: 25px;' align='center'>
+							<tr>
+								<td align='center'>
+									<table class='container' border='0' cellpadding='0' cellspacing='0' width='620' align='center' style='border-bottom: solid 1px #eeeeee; width: 620px;'>
+										<tr>
+											<td align='center'>&nbsp;</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+						</table>
+						<!-- /// Divider -->
+
+						<!-- / Info Bullets -->
+						<table class='container info-bullets' border='0' cellpadding='0' cellspacing='0' width='100%' align='center'>
+							<tr>
+								<td align='center'>
+									<table class='container' border='0' cellpadding='0' cellspacing='0' width='620' align='center' style='width: 620px;'>
+										<tr>
+											<td class='info-bullets__block' style='padding: 30px 30px 15px 30px;' align='center'>
+												<table class='container' border='0' cellpadding='0' cellspacing='0' align='center'>
+													<tr>
+														<td class='info-bullets__icon' style='padding: 0 15px 0 0;'>
+															<img src='https://cdn1.iconfinder.com/data/icons/material-communication/20/email-32.png'>
+														</td>
+
+														<td class='info-bullets__content' style='color: #969696; font-size: 16px;'>info@2dmarket.com</td>
+													</tr>
+												</table>
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+						</table>
+						<!-- /// Info Bullets -->
+
+						<!-- / Social nav -->
+						<table class='container' border='0' cellpadding='0' cellspacing='0' width='100%' align='center'>
+							<tr>
+								<td align='center'>
+									<table class='container' border='0' cellpadding='0' cellspacing='0' width='620' align='center' style='border-top: 1px solid #eeeeee; width: 620px;'>
+										<tr>
+											<td align='center' style='padding: 30px 0 30px 0;'>
+												<a href='https://www.facebook.com/2Dmarket'>
+													<img src='https://cdn2.iconfinder.com/data/icons/social-media-2151/512/1_Media_social_website_facebook-48.png' border='0'>
+												</a>
+											</td>
+
+											<td align='center' style='padding: 30px 0 30px 0;'>
+												<a href='https://twitter.com/2DmarKet1'>
+													<img src='https://cdn3.iconfinder.com/data/icons/picons-social/57/43-twitter-48.png' border='0'>
+												</a>
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+						</table>
+						<!-- /// Social nav -->
+
+						<!-- / Footer -->
+						<table class='container' border='0' cellpadding='0' cellspacing='0' width='100%' align='center'>
+							<tr>
+								<td align='center'>
+									<table class='container' border='0' cellpadding='0' cellspacing='0' width='620' align='center' style='border-top: 1px solid #eeeeee; width: 620px;'>
+										<tr>
+											<td style='text-align: center; padding: 50px 0 10px 0;'>
+												<a href='#' style='font-size: 28px; text-decoration: none; color: #d5d5d5;'>2D Market</a>
+											</td>
+										</tr>
+
+										<tr>
+											<td align='middle'>
+												<table width='60' height='2' border='0' cellpadding='0' cellspacing='0' style='width: 60px; height: 2px;'>
+													<tr>
+														<td align='middle' width='60' height='2' style=' width: 60px; height: 2px; font-size: 1px;'><img src='http://www.2dmarket.com/ar/images/logo_files/logo_png_email.png'></td>
+													</tr>
+												</table>
+											</td>
+										</tr>
+
+										<tr>
+											<td style='color: #d5d5d5; text-align: center; font-size: 15px; padding: 10px 0 60px 0; line-height: 22px;'>Copyright &copy; 2018 <a href='http://www.2dmarket.com/' target='_blank' style='text-decoration: none; border-bottom: 1px solid #d5d5d5; color: #d5d5d5;'>2D Market</a>. <br />All rights reserved.</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+						</table>
+						<!-- /// Footer -->
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
+";
+
+
+            $address1= $email_to_send;
+            $mail->AddAddress($address1);
+
+            $mail->Subject    = "مزايدة جديدة";
+
+            $mail->MsgHTML($body);
+
+            if(!$mail->Send()) {
+                echo "Mailer Error: " . $mail->ErrorInfo;
+            }
+
             echo "<meta http-equiv='refresh' content='0'>";
         }
 
@@ -277,75 +606,75 @@ if (isset($_GET['ad_id'])) {
 
 
         <div class="alert-box-container  margin-top-30">
-            <div class="well">
-                <h3>تاريخ المزايدات</h3>
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th>اسم المستخدم</th>
-                        <th>المبلغ</th>
-                        <th>منذ</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                    $get_auction_query = "SELECT * FROM `AUCTION` WHERE `ADVERTISEMENT_id`= '{$ad_id}'";
-                    $get_auction_result = mysqli_query($mysqli, $get_auction_query);
-                    if (mysqli_num_rows($get_auction_result) > 0) {
-                        while ($row = mysqli_fetch_assoc($get_auction_result)) {
-                            $auction_id = $row['id'];
-                        }
-                        $get_bid_query = "SELECT * FROM `BID` WHERE `AUCTION_id`= '{$auction_id}' ORDER BY `amount` DESC";
-                        $get_bid_result = mysqli_query($mysqli, $get_bid_query);
-                        while ($row = mysqli_fetch_assoc($get_bid_result)) {
-                            $bid_id = $row['id'];
-                            $amount = $row['amount'];
-                            $user_id = $row['user_id'];
-                            $time = $row['time'];
-                            require_once 'time_elapse.php';
-                            $time = time_elapsed_string($time);
+        <div class="well">
+            <h3>تاريخ المزايدات</h3>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>اسم المستخدم</th>
+                    <th>المبلغ</th>
+                    <th>منذ</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                $get_auction_query = "SELECT * FROM `AUCTION` WHERE `ADVERTISEMENT_id`= '{$ad_id}'";
+                $get_auction_result = mysqli_query($mysqli, $get_auction_query);
+                if (mysqli_num_rows($get_auction_result) > 0) {
+                    while ($row = mysqli_fetch_assoc($get_auction_result)) {
+                        $auction_id = $row['id'];
+                    }
+                    $get_bid_query = "SELECT * FROM `BID` WHERE `AUCTION_id`= '{$auction_id}' ORDER BY `amount` DESC";
+                    $get_bid_result = mysqli_query($mysqli, $get_bid_query);
+                    while ($row = mysqli_fetch_assoc($get_bid_result)) {
+                        $bid_id = $row['id'];
+                        $amount = $row['amount'];
+                        $user_id = $row['user_id'];
+                        $time = $row['time'];
+                        require_once 'time_elapse.php';
+                        $time = time_elapsed_string($time);
 
-                            $get_user_query = "SELECT * FROM `USER` WHERE id = '{$user_id}'";
-                            $get_user_result = mysqli_query($mysqli, $get_user_query);
-                            while ($row = mysqli_fetch_assoc($get_user_result)) {
-                                $username = $row['username'];
-                                ?>
-                                <tr>
-                                    <?php
-                                    if(isset($_SESSION['username'])){
-                                        ?>
-                                        <td <?php if ($username == $_SESSION['username']) { ?> style="color: green"<?php } ?>><?php echo $username; ?></td>
-
-                                        <?php
-                                    }else{
-                                        ?>
-                                        <td><?php echo $username; ?></td>
-
-                                        <?php
-                                    }
-                                    ?>
-                                    <td><?php echo $amount; ?></td>
-                                    <td><?php echo $time; ?></td>
-                                </tr>
-
+                        $get_user_query = "SELECT * FROM `USER` WHERE id = '{$user_id}'";
+                        $get_user_result = mysqli_query($mysqli, $get_user_query);
+                        while ($row = mysqli_fetch_assoc($get_user_result)) {
+                            $username = $row['username'];
+                            ?>
+                            <tr>
                                 <?php
-                            }
-                        }
-                        ?>
+                                if (isset($_SESSION['username'])) {
+                                    ?>
+                                    <td <?php if ($username == $_SESSION['username']) { ?> style="color: green"<?php } ?>><?php echo $username; ?></td>
 
-                        <?php
-                    } else { ?>
-                        <div role="alert" class="alert alert-warning alert-dismissible">
-                            <button aria-label="Close" data-dismiss="alert" class="close" type="button"></button>
-                            <strong>لا يوجد أي مزايدات حالياً</strong> قم بتقديم مزايدة لتكون الأول.
-                        </div>
-                        <?php
+                                    <?php
+                                } else {
+                                    ?>
+                                    <td><?php echo $username; ?></td>
+
+                                    <?php
+                                }
+                                ?>
+                                <td><?php echo $amount; ?></td>
+                                <td><?php echo $time; ?></td>
+                            </tr>
+
+                            <?php
+                        }
                     }
                     ?>
-                    </tbody>
-                </table>
-            </div>
 
+                    <?php
+                } else { ?>
+                    <div role="alert" class="alert alert-warning alert-dismissible">
+                        <button aria-label="Close" data-dismiss="alert" class="close" type="button"></button>
+                        <strong>لا يوجد أي مزايدات حالياً</strong> قم بتقديم مزايدة لتكون الأول.
+                    </div>
+                    <?php
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
+        <?php if (isset($_SESSION['username'])) { ?>
             <div class="well">
                 <h3>قم بالمزايدة الآن</h3>
                 <form action=" " method="post">
@@ -354,13 +683,23 @@ if (isset($_GET['ad_id'])) {
                             <input name="bid" placeholder="أدخل قيمة المزايدة" type="text" class="form-control">
                         </div>
                         <div class="col-md-3 col-xs-12 col-sm-12">
-                            <input name="submit" class="btn btn-theme btn-block" value="نشر" type="submit"">
+                            <input name="submit" class="btn btn-theme btn-block" value="نشر" type="submit">
                         </div>
                     </div>
                 </form>
             </div>
-        </div>
-    <?php } ?>
+            </div>
+            <?php
+        } else { ?>
+            <div role="alert" class="alert alert-warning alert-dismissible">
+                        <button aria-label="Close" data-dismiss="alert" class="close" type="button"></button>
+                        لتتمكن من المزايدة الرجاء قم <strong><a href="login.php"> بتسجيل الدخول</a></strong>
+                    </div>
+            <?php
+        }
+    }
+
+    ?>
     <!-- Single Ad End -->
 
     <!-- =-=-=-=-=-=-= Latest Ads =-=-=-=-=-=-= -->
