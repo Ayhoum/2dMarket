c2b9fd80f653c0f5d2eee1dace303388ee7d514f
Ayham <aylosa@outlook.com>
2018-05-24 23:38:29 +0200

    Ayham

--- nl/index.php
+++ nl/index.php
@@ -317,6 +317,7 @@ require_once "scripts/time_elapse.php";
       <script src="js/forest-megamenu.js"></script>
       <!-- Jquery Appear Plugin -->
       <script src="js/jquery.appear.min.js"></script>
+      <script src="js/jquery.flexslider-min.js"></script>
       <!-- Numbers Animation   -->
       <script src="js/jquery.countTo.js"></script>
       <!-- Jquery Smooth Scroll  -->
@@ -413,7 +414,6 @@ require_once "scripts/time_elapse.php";
           myLon = getCookie("longC");
 
       </script>
-      <script src="js/jquery.flexslider-min.js"></script>
    </body>
 </html>
 
